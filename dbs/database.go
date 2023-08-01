085efe262c340a5e5c2a6fa395578db0cc24c772
Zoey Zuo <zoeyzuouk@gmail.com>
2023-08-01 15:02:15 +0100

    Switch to Azure SQL Database

--- dbs/database.go
+++ dbs/database.go
@@ -2,10 +2,16 @@ package dbs
 
 import (
 	"log"
+	"os"
 
 	"github.com/meal-planner/models"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+
+	"fmt"
+
+	"gorm.io/driver/sqlserver"
+
+	_ "github.com/microsoft/go-mssqldb"
 )
 
 type DbInstance struct {
@@ -15,14 +21,26 @@ type DbInstance struct {
 var Database DbInstance
 
 func ConnectDb() error {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	var err error
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_DATABASE")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	// Build connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;database=%s;",
+		dbHost, dbUsername, dbPassword, dbPort, dbName)
+
+	// Create connection pool
+	db, err := gorm.Open(sqlserver.Open(connString), &gorm.Config{})
 	if err != nil {
 		log.Fatal("failed to connect database \n", err.Error())
 		return err
 	}
-
-	log.Println("Database connection successfully opened")
 	db.AutoMigrate(&models.Recipe{})
 	Database.Db = db
+	fmt.Printf("Connected!")
 	return nil
 }
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/meal-planner/api"
 	"github.com/meal-planner/dbs"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	if err := dbs.ConnectDb(); err != nil {
 		panic(err)
 	}
